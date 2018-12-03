6acd6d6ec136b918ba3558cbb57a8aabae220ccf
Stijn <stijn.saenen@student.ucll.be>
2018-12-03 09:45:58 +0100

    Stijn Intellij Test

--- src/view/panels/MessagePane.java
+++ src/view/panels/MessagePane.java
@@ -31,7 +31,7 @@ public class MessagePane extends GridPane {
 			
 			@Override
 			public void handle(ActionEvent event) {
-				//stijn test
+				//stijn test 1
 			}
 		});
 		add(testButton, 0,1,1,1);
